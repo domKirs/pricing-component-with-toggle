f0e502e6f5fa98a81c7896c96c459430c6fa712c
Dominic Kirste <dominic.kirste@proleit.com>
2020-06-05 12:36:22 +0200

    observer pattern + update für toggle

--- script.js
+++ script.js
@@ -1,21 +1,64 @@
-customElements.define('toggle-button', 
-    class extends HTMLElement {
-        constructor() {
-            super();
-            const template = document.getElementById('toggle-button');
-            const shadowRoot = this.attachShadow({mode: 'open'});
-            shadowRoot.appendChild(template.content.cloneNode(true));
-        }
+class Subject {
+  constructor() {
+    this.observers = [];
+  }
+
+  addObserver(observer) {
+    this.observers.push(observer);
+  }
+
+  removeObserver(observer) {
+    const removeIndex = this.observers.findIndex(obs => obs === observer);
+    if (removeIndex !== -1) {
+      this.observers = this.observers.slice(removeIndex, 1);
     }
+  }
+
+  notify() {
+    if (this.observers.length > 0) {
+      this.observers.forEach(obs => obs.update());
+    }
+  }
+}
+
+const clickSubject = new Subject();
+
+customElements.define('toggle-button', 
+  class extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({mode: 'open'});
+    }
+    
+    connectedCallback() {
+      const template = document.getElementById('toggle-button');
+      this.shadowRoot.appendChild(template.content.cloneNode(true));
+      this.addEventListener('click', e => clickSubject.notify());
+    }
+
+  }
 );
 
 customElements.define('pricing-card',
-    class extends HTMLElement {
-        constructor() {
-            super();
-            const template = document.getElementById('pricing-card');
-            const shadowRoot = this.attachShadow({mode: 'open'});
-            shadowRoot.appendChild(template.content.cloneNode(true));
-        }
+  class extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({mode: 'open'});
+      clickSubject.addObserver(this);
     }
+    
+    connectedCallback() {
+      const template = document.getElementById('pricing-card');
+      this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    update() {
+      const slot = this.querySelector('[slot="pricing"]');
+      const price = Number(slot.innerHTML);
+      const isAnnually = slot.hasAttribute('annually');
+      const newPrice = isAnnually ? price / 11 : price * 11;
+      slot.toggleAttribute('annually');
+      slot.innerHTML = newPrice.toFixed(2);
+    }
+  }
 );
