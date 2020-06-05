ace2edad5ea8fe6ab5873ffa55e4ad0c27d0c92d
Dominic Kirste <dominic.kirste@proleit.com>
2020-06-05 09:39:29 +0200

    web component: pricing-card erstellt

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 customElements.define('toggle-button', 
-    class extends HTMLElement{
+    class extends HTMLElement {
         constructor() {
             super();
             const template = document.getElementById('toggle-button');
+            const shadowRoot = this.attachShadow({mode: 'open'});
+            shadowRoot.appendChild(template.content.cloneNode(true));
+        }
+    }
+);
+
+customElements.define('pricing-card',
+    class extends HTMLElement {
+        constructor() {
+            super();
+            const template = document.getElementById('pricing-card');
             const shadowRoot = this.attachShadow({mode: 'open'});
             shadowRoot.appendChild(template.content.cloneNode(true));
         }
